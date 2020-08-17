d018a8dcacb4410b7e6cf30883687a03c98507a9
abdulhannan527 <67515318+abdulhannan527@users.noreply.github.com>
2020-08-17 12:37:58 +0800

    now is done

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
-
-
-let appId = '1acec66c004ce56b26a08261c4c237f5';
-
-document.getElementById('searchBtn').addEventListener('click',function(){
-
-    const cityName = document.getElementById('cityName').value;
-    // fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${appId}`)
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${appId}`)
-    .then(res => res.json())
-    .then(data => {
-        getAllInfo(data);
-    })
-});
-
-function getAllInfo(data){
-    console.log(data);
-    document.getElementById('city').innerText = data.name;
-
-    document.getElementById('temperature').innerText = Math.round(data.main.temp - 273.15);
-
-    document.getElementById('lead').innerText = data.weather[0].main;
-
-    const iconCode = data.weather[0].icon;
-    const iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
-    document.getElementById('icon').src = iconUrl;
+function showWeatherData(data) {
+	if (!data) {
+		return;
+	}
+	document.getElementById(
+		"weather-icon"
+	).src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+	document.getElementById("cityName").innerText = data.name;
+	document.getElementById("country").innerText = data.sys.country;
+	document.getElementById("temperature").innerText = data.main.temp;
+	document.getElementById("weather-condition").innerText = data.weather[0].main;
+	document.getElementById("feels_like").innerText = data.main.feels_like;
+	document.getElementById("max_temp").innerText = data.main.temp_max;
+	document.getElementById("min_temp").innerText = data.main.temp_min;
+	document.getElementById("humidity").innerText = data.main.humidity;
+	document.getElementById("wind_speed").innerText = data.wind.speed;
+	document.getElementById("sunrise").innerText = new Date(
+		data.sys.sunrise * 1000
+	).toLocaleTimeString();
+	document.getElementById("sunset").innerText = new Date(
+		data.sys.sunset * 1000
+	).toLocaleTimeString();
 }
+function fetchWeatherData(city) {
+	fetch(
+		`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${"1acec66c004ce56b26a08261c4c237f5"}&units=metric`
+	)
+		.then((response) => response.json())
+		.then((json) => showWeatherData(json))
+		.catch((error) => alert("Not found"));
+}
+document.getElementById("search").addEventListener("click", function () {
+	const city = document.getElementById("city").value;
+	fetchWeatherData(city);
+});
+fetchWeatherData("dhaka");
